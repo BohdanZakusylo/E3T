b23eb140b4b01347335fde86a79d3ebdf68c7a23
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2022-12-08 15:08:32 +0100

    Added the footer

--- app/public/components/footer.php
+++ app/public/components/footer.php
@@ -1,3 +1,7 @@
-<!--Conatins the footer part-->
-
-
+<footer>
+    <h2>Want to reach the team?</h2>
+    <div id="contact_us">Contact us</div>
+    <h4>Powered by E3T Technology. All rights reserved</h4>
+</footer>
+</body>
+</html>
