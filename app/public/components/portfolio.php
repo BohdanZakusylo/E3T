9230086c95a412e838368064987e1ea16acd0b1e
Mich Ioan <ioan.mich@student.nhlstenden.com>
2022-12-08 19:31:02 +0100

    Modified portofolio.php and added favicon to img folder

--- app/public/components/portfolio.php
+++ app/public/components/portfolio.php
@@ -1,1 +1,20 @@
-<!--Contains the portfolio part-->
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="../css/portfolio.css">
+</head>
+<body>
+    <div class="main_container">
+        <img src="../img/logo.png">
+        <h2>Mike Philips</h2>
+        <h4>Description</h4>
+        <h6>.</h6>
+        <h6>.</h6>
+        <h6>.</h6>
+        <hr>
+        <br><br>
+        <h4>Rating:☆☆☆☆☆</h4>
+    </div>
+</body>
+</html>
+
