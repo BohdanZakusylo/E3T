a0aafa4e52a95cbcf11a6b5a850501029c3e06e8
AdaBilici <ada.bilici@student.nhlstenden.com>
2022-12-12 12:18:32 +0100

    added JavaScript, header and footer

--- app/public/index.php
+++ app/public/index.php
@@ -1,7 +1,7 @@
     <?php
     $cssFile = "index";
     $pageTitle = "E3T";
-    include "../components/header.php";
+    include "components/header.php";
     ?>
 
     <!DOCTYPE html>
@@ -10,16 +10,16 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="css/index.css">
-        <link rel="stylesheet" type="text/css" href="css/global.css">
+        <link rel="stylesheet" type="text/css" href="../css/index.css">
+        <link rel="stylesheet" type="text/css" href="../css/global.css">
         <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+        <link rel="stylesheet" href="css/swiper-bundle.min.css">
+         <link rel="stylesheet" href="css/style.css">
         
         <title>E3T - Home </title>
     </head>
-    <body>
-        <main>
-            <div class="banner">
-                    <img src="../img/banner.png" alt="banner">
+    <main>
+            <div id="banner">
                             <div class="textBanner">
                                 <h1>Organise successful events<br> and discover talents with E3T</h1>
                                 <div id="subTitle">
@@ -29,15 +29,47 @@
                                         <a href="events.php"><b>Discover Talents</b></a>
                                     </div>
                         </div>
-                            
-                                
+</div>              
 
-        </main>
+</main> 
+
+
         <section>
             <div id="events">
                 <h1>Upcoming Events</h1>
             </div>
-                <div class="upcoming">
+
+            <div class="slide-container swiper">
+        <div class="slide-content">
+        <div class="card-wrapper swiper-wrapper">
+        <?php
+
+            for ($i=0; $i<9; $i++) {
+            echo ' <div class="card swiper-slide">
+            <div class="image-content">
+                    <src="#">
+                </div>
+
+
+            <div class="event-content">
+                <h2 class="name">Name of Event</h2>
+                <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                <button class="button">View Event</button>
+            </div>
+        </div>';
+}
+?>
+        </div>
+    </div>
+    <div class="swiper-button-next swiper-navBtn"></div>
+            <div class="swiper-button-prev swiper-navBtn"></div>
+            <!-- <div class="swiper-pagination"></div> -->
+</div>
+
+
+
+                <!-- <div class="upcoming">
                     <div class="arrow">
                         <i class="fas">&#xf104;</i>
                     </div>
@@ -76,14 +108,15 @@
                     </div>    
                         <div class="arrow">
                             <i class="fas">&#xf105;</i>
-                        </div>
+                        </div> -->
 
                 
             </section>
-                
-        </body>
+             
+            <script src="js/swiper-bundle.min.js"></script>
+             <script src="js/script.js"></script>
         </html>
 
 <?php
-include "../components/footer.php";
+include "components/footer.php";
 ?>
