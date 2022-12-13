4f3293f282e1e975209db78aa7b8525245fa7f10
AdaBilici <ada.bilici@student.nhlstenden.com>
2022-12-13 14:36:46 +0100

    modified the index.php
    adding javascript, fix the problem with background img, modified css, added grids, added slider (js)

--- app/public/index.php
+++ app/public/index.php
@@ -1,27 +1,27 @@
-    <?php
+<?php
     $cssFile = "index";
     $pageTitle = "E3T";
     include "components/header.php";
     ?>
 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="../css/index.css">
-        <link rel="stylesheet" type="text/css" href="../css/global.css">
-        <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
-        <link rel="stylesheet" href="css/swiper-bundle.min.css">
-         <link rel="stylesheet" href="css/style.css">
-        
-        <title>E3T - Home </title>
-    </head>
-    <main>
-            <div id="banner">
-                            <div class="textBanner">
-                                <h1>Organise successful events<br> and discover talents with E3T</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E3T - Home</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" href="css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+<main>
+    <div id="top-background">
+
+            <div class="textBanner">
+                 <h1>Organise successful events<br> and discover talents with E3T</h1>
                                 <div id="subTitle">
                                     <p>Discover talents and organise events in Emmen and<br> surroundings</p>
                                 </div>
@@ -29,12 +29,12 @@
                                         <a href="events.php"><b>Discover Talents</b></a>
                                     </div>
                         </div>
-</div>              
 
-</main> 
+    </div>
 
+</main>
 
-        <section>
+<section>
             <div id="events">
                 <h1>Upcoming Events</h1>
             </div>
@@ -64,54 +64,12 @@
     </div>
     <div class="swiper-button-next swiper-navBtn"></div>
             <div class="swiper-button-prev swiper-navBtn"></div>
-            <!-- <div class="swiper-pagination"></div> -->
+
 </div>
 
+</section>
 
-
-                <!-- <div class="upcoming">
-                    <div class="arrow">
-                        <i class="fas">&#xf104;</i>
-                    </div>
-
-
-                        <div class="fullBox">
-                            <div class="box">
-                                <h2>placeholder</h2>
-                            </div>
-                                <div class="boxTitle">
-                                    <h3>School Tour</h3>
-                                    <h3>10:00</h3>
-                                </div>
-                                    <div class="boxDescription">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing
-                                                    elit Lorem ipsum dolor sit amet, consectetur
-                                                    adipiscing elit
-                                                </p>
-                                    </div>
-
-                                    <div class="fullBox">
-                            <div class="box">
-                                <h2>placeholder</h2>
-                            </div>
-                                <div class="boxTitle">
-                                    <h3>School Tour</h3>
-                                    <h3>10:00</h3>
-                                </div>
-                                    <div class="boxDescription">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing
-                                                    elit Lorem ipsum dolor sit amet, consectetur
-                                                    adipiscing elit
-                                                </p>
-                                    </div>
-                        </div>
-                    </div>    
-                        <div class="arrow">
-                            <i class="fas">&#xf105;</i>
-                        </div> -->
-
-                
-            </section>
+    </body>
              
             <script src="js/swiper-bundle.min.js"></script>
              <script src="js/script.js"></script>
