52abb3e9cf9e9cade864569cb72967a27ddd6e50
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2022-12-19 14:32:06 +0100

    Added the redirect to a new page where all the information about the event is shown

--- app/public/event-info.php
+++ app/public/event-info.php
@@ -1,18 +1,48 @@
 <?php
+$cssFile = "event-info";
+$pageTitle = "Event info";
+include "./components/header.php";
+?>
+
+<?php
 try {
     $db = new PDO("mysql:host=mysql;dbname=E3T;charset=utf8","root", "qwerty");
 }
 catch (Exception $ex){
     echo "$ex";
 }
-?>
-<?php
-$cssFile = "events";
-$pageTitle = "Events";
-include "./components/header.php";
-?>
 
+if (!empty($_GET["id"])){
+    try {
+        $outputs = $db->prepare("SELECT * FROM Events WHERE event_id = :id");
+        $outputs->bindParam(":id", $_GET["id"]);
+        $outputs->execute();
+        while($output = $outputs->fetch()){
+            echo "<main>";
+            echo "<div class='message1'>";
+            echo "<b>Name of the event:</b> ".$output['name']."<br>";
+            echo "<b>Event start on: </b> ".$output['start_date']."<br>";
+            echo "<b>Event ends on: </b> ".$output['end_date']."<br>";
+            echo "<b>Time: </b> ".$output['time']."<br>";
+            echo "<b>Event take place in: </b> ".$output['place']."<br>";
+            echo "<b>Description: </b> ".$output['event_description']."<br>";
+            echo "<br>";
+            echo "<button><a href='events.php'>Go Back</a></button>";
+            echo "</div>";
+            echo "</main>";
+        }
+    }
+    catch (Exception $ex){
+        echo "The id is invalid, try again later";
+        die();
+    }
 
+}
+else{
+    echo "The page is invalid, try again later";
+}
+
+?>
 
 <?php
 include "./components/footer.php";
