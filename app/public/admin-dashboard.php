a369d96b8c702785ce35385ad5f4a4daa92d1006
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2022-12-20 16:34:29 +0100

    Deleted the dublication on index.php and made the talents.php page

--- app/public/admin-dashboard.php
+++ app/public/admin-dashboard.php
@@ -20,15 +20,6 @@ catch(Exception $ex){
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-</head>
-<body>   
         <div class="header">
             <h2>Admin Dashboard</h2>
         </div>
@@ -159,8 +150,6 @@ catch(Exception $ex){
             </div>
         </div>
 
-</body>
-</html>
 
 <?php
 include "components/footer.php";
--- app/public/components/portfolio.php
+++ app/public/components/portfolio.php
@@ -1,19 +1,18 @@
 <?php
-function generate_portfolio(){
+function generate_portfolio($url, $first_name, $last_name,$talent, $description, $price, $available){
      echo '<head>
         <link rel="stylesheet" type="text/css" href="../css/portfolio.css">
     </head>
         <div class="main_container">
-            <img src="../img/image.png">
-            <h2>Mike Philips</h2>
-            <div id="description">    
-                <h4>Description</h4>
-                <h6>.</h6>
-                <h6>.</h6>
-                <h6>.</h6>
+            <img src="../img/'.$url.'">
+            <h2>'.$first_name.' '.$last_name.'</h2>
+            <div id="description"> 
+                <h4>Talent: '.$talent.'</h4>   
+                <h4>'.$description.'</h4>
             </div>
             <br><br>
-            <h4>Rating:</h4>
+            <h4>Price: '.$price.'</h4>
+            <h4>Available: '.$available.'</h4>
         </div>';
     }
 ?>
