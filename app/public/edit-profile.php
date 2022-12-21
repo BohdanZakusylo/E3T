2934b78e547ea488b62a35d08c41740657c542d4
AdaBilici <ada.bilici@student.nhlstenden.com>
2022-12-21 09:44:30 +0100

    HTML/CSS

--- app/public/edit-profile.php
+++ app/public/edit-profile.php
@@ -21,15 +21,13 @@ include ("components/header.php");
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit profile</title>
 </head>
 <body>
 <main>
 <section>
-    <div>
-        <img src="components/cover/jpg">
-    </div>
 </section>
 
 <sub-section>
@@ -48,33 +46,44 @@ include ("components/header.php");
         Edit profile
         </h3></b>
             <h2>Full name:</h2>
+            <form action="edit-profile.php" method="POST" enctype="multipart/form-data">
             <input type="text" name="full_name" id="full_name" placeholder="Enter your full name here"><br>
-  <div class="description">
+            <input type="submit" name="edit_name" value="Edit name"></input>
+
     <h4>Description</h4>
     <!-- php code to replace this lorep ipsum below -->
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-</p>
-    </div>   
-    
+    <input type="text" name="description" id="description" placeholder="Edit your description"><br>
+    <input type="submit" name="edit_description" value="Edit description"></input>
+
+
     <div class="upload">
     <form action="edit-profile.php" method="POST" enctype="multipart/form-data">
 
                 
+<br><br>
+    <label for="file-upload" class="custom-file-upload">
+    <i class="fa fa-cloud-upload"></i> Upload Profile Image
+  </label>
+  <input id="file-upload" name='upload_cont_img' type="file" style="display:none;">
+  <input type="submit" name="edit_image" value="Edit profile picture"></input>
 
-            <input type="file" id="upload" hidden/>
-            <label for="upload"><p>Upload Profile Picture<p></label>
+  <?php 
+  
 
 
+  ?>
+  
+
+<script>
+    $('#file-upload').change(function() {
+  var i = $(this).prev('label').clone();
+  var file = $('#file-upload')[0].files[0].name;
+  $(this).prev('label').text(file);
+});
+</script>
 
                 
     </div>
-        <div class="upload">
-            <input type="file" id="upload" hidden/>
-            <label for="upload"><p>Upload Cover Image</p></label>
-
-        </div>
-        <input type="submit" name="submit" value="submit"></input>
 </form>
 </div>
 
