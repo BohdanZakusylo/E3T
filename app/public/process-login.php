03aa922f3b27179a6fe04da9803687c7c3e775d0
michaelehiosu <michael.ehiosumwen@student.nhlstenden.com>
2023-01-06 14:35:01 +0100

    Added the login file to my branch

--- app/public/process-login.php
+++ app/public/process-login.php
@@ -8,7 +8,7 @@ catch(Exception $err){
     echo "<p class='error'>$err</p>";
 }
 
-if($_SERVER["REQUEST_METHOD"]=="POST"){
+if($_SERVER["REQUEST_METHOD"]=="POST" AND $_GET["login"]==="admin"){
     $email = filter_input(INPUT_POST,"email",FILTER_VALIDATE_EMAIL); #Get the email and password from the form
     $pass = filter_input(INPUT_POST,"password",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
@@ -27,17 +27,55 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 
         if(password_verify($pass,$passHash)){ # Verifies user password against retrieved hash
             $_SESSION["aLogin"] = true;
+            $_SESSION['id'] = $queryResult[0][0];
+            setcookie("user_id",$queryResult[0][0],time()+10000);
             header("Location: admin-dashboard.php");
         }
         else{
-            header("Location: admin-dashboard.php?error=PW"); #If pw is invalid, redirects to login page
+            header("Location: admin-login.php?error=PW"); #If pw is invalid, redirects to login page
         }
     }
     else{  #Invalid email
 
-        header("Location: admin-dashboard.php?error=EM");
+        header("Location: admin-login.php?error=EM");
     }
 
 
+}
+elseif($_SERVER["REQUEST_METHOD"]=="POST" AND $_GET["login"]==="talent"){
+
+    $email = filter_input(INPUT_POST,"email",FILTER_VALIDATE_EMAIL); #Get the email and password from the form
+    $pass = filter_input(INPUT_POST,"password",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    try{
+        $stmt = $dbHandler -> prepare("SELECT id, password FROM Talent WHERE email = :email"); #Tries to find email in DB
+        $stmt -> bindParam("email",$email,PDO::PARAM_STR);
+        $stmt -> execute();
+        $queryResult = $stmt -> fetchAll();
+        
+    }
+    catch(Exception $err){
+       echo "<p class='error'>$err</p>";
+    }
+    if($queryResult){  #Valid email
+        $passHash = $queryResult[0][1]; #Password hash retrieved from DB
+
+        if(password_verify($pass,$passHash)){ # Verifies user password against retrieved hash
+            $_SESSION["tLogin"] = true;
+            setcookie("user_id",$queryResult[0][0],time()+10000);
+            header("Location: talent-dashboard.php");
+        }
+        else{
+            header("Location:login.php?error=PW"); #If pw is invalid, redirects to login page
+        }
+    }
+    else{  #Invalid email
+
+        header("Location: login.php?error=EM");
+    }
+}
+else{
+    echo "Well this is not supposed to happen";
+    header("Location: login.php");
 }
 ?>
