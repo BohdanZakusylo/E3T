e673899a43e72adca31cea732a9c55926832205c
Mich Ioan <ioan.mich@student.nhlstenden.com>
2023-01-09 19:14:19 +0100

    Made most of the php for adding events and remove talents, still working on add photo to new talents

--- app/public/manage_events.php
+++ app/public/manage_events.php
@@ -1,19 +1,15 @@
 <?php
 	
-	if($_SERVER['REQUEST_METHOD']=='POST'){
-		// $event_name=filter_input(INPUT_POST, "name_event");
-		// $start_date=filter_input(INPUT_POST, "start_date");
-		// $end_date=filter_input(INPUT_POST, "end_date");
-		// $time=filter_input(INPUT_POST, "time");
-		// $place=filter_input(INPUT_POST, "place");
-		// $description=filter_input(INPUT_POST, "description");
-		// $performing_talent=filter_input(INPUT_POST, "performing_talent");
-	}
+	$db= new PDO("mysql:host=mysql;dbname=E3T;charset=UTF8", "root", "qwerty");
 	
 	if(isset($_GET["add"])){
+		if($_SERVER['REQUEST_METHOD']=='POST'){
+				$uploadedFileType = finfo_file($fileinfo, $_FILES["uploadfile"]["img"]);
+		}
+		
+		
 		if($_GET['add']=="true"){
 			try{
-				$db= new PDO("mysql:host=mysql;dbname=E3T;charset=UTF8", "root", "qwerty");
 				$query=$db->prepare("INSERT INTO Events VALUES(NULL, :event_name, :start_date, :end_date, :time, NULL, NULL, :place, :description, 0)");
 				
 				$query->bindParam(':event_name', $_REQUEST['name_event']);
@@ -23,12 +19,25 @@
 				$query->bindParam(':place', $_REQUEST['place']);
 				$query->bindParam(':description', $_REQUEST['description']);
 				
-				
 				$query->execute();
+				echo"<h2>Talent added</h2>";
 			}catch(Exception $ex){
 				echo $ex;
 			}
 		}
+	}
+	$count_id=$db->query("SELECT max(event_id) FROM Events");
+	$id=$count_id->fetch();
+	
+	$delete=$db->query("SELECT name FROM Events");
+	$event_delete=$delete->fetch();
+	
+	if(isset($_GET["delete"])){
+		if($_GET['delete']=="1")
+			$delete_event_query=$db->prepare("DELETE FROM Events WHERE name=:deleted");
+			$delete_event_query->bindParam(':deleted', $_POST['delete_talent']);
+			$delete_event_query->execute();
+			echo"<h2>Talent deleted</h2>";
 	}
 ?>
 
@@ -83,7 +92,7 @@
 						<input type="text" name="performing_talent" class="input_text" required>
 					</div>
 					
-					<input type="file">
+					<input type="file" name="event_photo">
 					
 					<div>
 						<button type="submit">Register event</button>
@@ -95,10 +104,19 @@
 			<div class="delete_events">
 				<div class="spacing">
 					<h2>Delete event</h2>
-					<form action="" method="POST" class="form_margin">
+					<form action="manage_events.php?delete=1" method="POST" class="form_margin">
 						<label for="delete_talent" class="label">Name of event</label> <br>
 						<select name="delete_talent" class="input_text" required>
-							<option>PHP HERE</option>
+						<option></option>
+							<?php
+								for($i=0;$i<=$id[0];$i++){
+									$delete=$db->query("SELECT name FROM Events WHERE event_id=".$i);
+									$event_delete=$delete->fetch();
+									if($event_delete){
+										echo "<option>".$event_delete['name']."</option>";
+									}
+								}
+							?>
 						</select>
 						<div class="delete_button">
 							<button type="submit">Delete Event</button>
@@ -109,7 +127,7 @@
 		</div>
 		
 		<div class="go_back">
-			<a href="" class="back">Go back</a>
+			<a href="index.php" class="back">Go back</a>
 		</div>
 		
 	</body>
