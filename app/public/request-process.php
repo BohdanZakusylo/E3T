aec703128553a455362965359684fefcd856677a
PeterRubint <peter.rubint@student.nhlstenden.com>
2023-01-10 11:40:31 +0100

    Added password hashing

--- app/public/request-process.php
+++ app/public/request-process.php
@@ -5,16 +5,19 @@
 
     include "db_connection/connection.php";
     if (!empty($_GET)){
+
+        $pass_hash = password_hash("root",PASSWORD_BCRYPT);
         $outputs = $db->prepare("SELECT  first_name, last_name, talent, email, description FROM Requests WHERE request_id = :request_id");
         $outputs->bindParam(":request_id", $_GET["id"]);
         $outputs->execute();
         while($output = $outputs->fetch()) {
             $insert = $db->prepare("INSERT INTO Talent(description, email, first_name, last_name, password, price_per_hour, profilepic_url, rating, talent) 
-            VALUES(:description, :email, :first_name, :last_name, 'root', 50, '', 1, :talent)");
+            VALUES(:description, :email, :first_name, :last_name, :pass_hash, 50, '', 1, :talent)");
             $insert->bindParam(":description", $output["description"]);
             $insert->bindParam(":email", $output["email"]);
             $insert->bindParam(":first_name", $output["first_name"]);
             $insert->bindParam(":last_name", $output["last_name"]);
+            $insert->bindParam(":pass_hash",$pass_hash,PDO::PARAM_STR);
             $insert->bindParam(":talent", $output["talent"]);
             $insert->execute();
             $delete = $db->prepare("DELETE FROM Requests WHERE request_id = :id");
