a7c0ef0bfa1ac4688d1997f06cc19c948fe2c4da
michaelehiosu <michael.ehiosumwen@student.nhlstenden.com>
2023-01-11 16:13:23 +0100

    I added the performing talent feature

--- app/public/event-info.php
+++ app/public/event-info.php
@@ -36,7 +36,7 @@ if (!empty($_GET["id"])){
                 $value = $stmt->fetch();
 
                 if($value) {
-                    echo "<b>Talent: </b> ".$value['talent']."<br>";
+                    echo "<b>Performing Talent: </b> ".$value['talent']."<br>";
                 } else {
                     echo "Couldn't fetch from the DB";
                 }
